06509d72b66a98a85ff22458dd688c4d69fda6bb
GabrielGirotti <girottigabriel@gmail.com>
2024-06-22 09:04:39 -0300

    starting project and new components

---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function BudgetForm() {
+  const [budget, setBudget] = useState(0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBudget(+e.target.value);
+  };
+
+  return (
+    <form className=" space-y-5">
+      <div className=" flex flex-col space-y-5">
+        <label
+          htmlFor="budget"
+          className=" text-4xl text-blue-600 font-bold text-center"
+        >
+          Definir presupuesto
+        </label>
+        <input
+          type="number"
+          className=" w-full bg-white border border-gray-200 p-2"
+          placeholder="Introduzca su presupuesto..."
+          name="budget"
+          id="budget"
+          value={budget}
+          onChange={handleChange}
+        />
+      </div>
+
+      <input
+        type="submit"
+        value="Definir presupuesto"
+        className=" bg-blue-600 hover:bg-blue-700 cursor-pointer text-white uppercase w-full p-2 font-bold"
+      />
+    </form>
+  );
+}
